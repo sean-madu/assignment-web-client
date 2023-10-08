2fd0c7e6d2b3e9661b19853ae1a1819707f65bc9
sean-madu <madusean2@gmail.com>
2023-10-08 02:49:24 -0600

    c

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,16 +24,19 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    # def get_host_port(self,url):
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,15 +46,15 @@ class HTTPClient(object):
     def get_code(self, data):
         return data.split(" ")[1]
 
-    def get_headers(self,data):
+    def get_headers(self, data):
         return data.split("\r\n\r\n")[0]
 
     def get_body(self, data):
         return data.split("\r\n\r\n")[1]
-    
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        
+
     def close(self):
         self.socket.close()
 
@@ -72,7 +75,9 @@ class HTTPClient(object):
         port = 80
         p_url = urllib.parse.urlparse(url)
         host = p_url.netloc
-        if p_url.path != "/":
+        if p_url.path.endswith("/"):
+            host += p_url.path[0:len(p_url.path) - 1]
+        else:
             host += p_url.path
         if p_url.query != "":
             host += "?" + p_url.query
@@ -86,8 +91,9 @@ class HTTPClient(object):
         try:
             self.connect(host, port)
         except:
-            self.connect(host, port)
+
             print(f"Could not connect to host: {host}")
+            self.connect(host, port)
 
             return HTTPResponse(404, '')
         self.sendall(payload)
@@ -97,7 +103,7 @@ class HTTPClient(object):
         headers = self.get_headers(content)
 
         if (code.startswith("3")):
-            #this is a redirect code
+            # this is a redirect code
             for header in headers.split("\n"):
                 if header.upper().startswith("LOCATION"):
                     location = header.split(" ")[1]
@@ -140,10 +146,11 @@ class HTTPClient(object):
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
-            return self.POST( url, args )
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
-    
+            return self.GET(url, args)
+
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
@@ -151,6 +158,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command(sys.argv[2], sys.argv[1]))
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
